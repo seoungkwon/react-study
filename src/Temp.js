8b5060e9e166be03d4444c3b35467e1b2a87c7e0
seoungkwon <s2ngkwon2@naver.com>
2025-05-21 12:03:07 +0900

    리엑트 기본 JSX, 컴포넌트 연결

---
+++ src/Temp.js
@@ -0,0 +1,16 @@
+
+
+function Temp(){
+
+    return (
+        <div>
+            <h1>Temp파일</h1>
+            <h2>Temp 컴포넌트</h2>
+            <h3>내용 텍스트</h3>
+        </div>
+    );
+    
+}
+
+export default Temp;
+
