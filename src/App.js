7195d00555998cba81cd796c5bdfaefdb25659f0
seoungkwon <s2ngkwon2@naver.com>
2025-05-21 15:04:22 +0900

    리액트 useState 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Basic01 from './study/Basic01';
+import State01 from './study/State01';
+import Practice01 from './Practice/Practice01';
+
+
 
 function App() {
   
-  return <Basic01/>;
+  // return <Basic01/>; //JSX 기본
+  // return <State01/>;  //useState 사용법
+  return <Practice01/>;
 
   /*
   return (
