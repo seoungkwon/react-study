b3350d87d0d820875a6812749d8470b6f99cde07
seoungkwon <s2ngkwon2@naver.com>
2025-05-22 17:14:05 +0900

    모달창 처리, 모달창 props 전달

--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -1,5 +1,6 @@
 import './NewsBlog.css';
 import { useState } from 'react';
+import Modal from './Modal';
 
 function NewsBlog() {   
     
@@ -16,7 +17,12 @@ function NewsBlog() {
     let [likeCount2, setLikeCount2] = useState(0);
     let [likeCount3, setLikeCount3] = useState(0);
 
-    let [likeCounArr, setLikeCountArr] = useState( [0, 0, 0] );
+    let [likeCountArr, setLikeCountArr] = useState( [0, 0, 0] );
+
+    let[showModal, setShowModal] = useState(false);
+    //       0           1
+
+    let [selectedTitle, setSelectedTitle] = useState('');
 
     return (
         <div>
@@ -27,14 +33,22 @@ function NewsBlog() {
 
             <div className='post-list'>
                 {/* <h4>{news1}</h4> */}
-                <h4>{news[0]} <span onClick={()=>{
+                <h4 onClick={()=>{
+                    if(showModal)
+                        setShowModal(false);
+                    else
+                        setShowModal(true);
+
+                    setSelectedTitle(news[0]);
+
+                }}>{news[0]} <span onClick={()=>{
 
                     // setLikeCount1(likeCount1+1);
-                    let temp = [...likeCounArr];
+                    let temp = [...likeCountArr];
                     temp[0] += 1;
                     setLikeCountArr(temp);
 
-                }}>💋</span>{likeCounArr[0]}</h4>
+                }}>💋</span>{likeCountArr[0]}</h4>
                 <p>내용 무</p>
 
                 
@@ -51,29 +65,47 @@ function NewsBlog() {
 
             <div className='post-list'>
                 {/* <h4>{news1}</h4> */}
-                <h4>{news[1]} <span onClick={()=>{
+                <h4 onClick={()=>{
+
+                    setShowModal(!showModal);
+                    setSelectedTitle(news[1]);
+                    //           !true -> false
+                    //           !false -> true
+
+                }}>{news[1]} <span onClick={()=>{
 
                     // setLikeCount2(likeCount2+1);
-                    let temp = [...likeCounArr];
+                    let temp = [...likeCountArr];
                     temp[1] += 1;
                     setLikeCountArr(temp);
 
-                }}>💋</span>{likeCounArr[1]}</h4>
+                }}>💋</span>{likeCountArr[1]}</h4>
                 <p>내용 무</p>
             </div>
 
             <div className='post-list'>
                 {/* <h4>{news1}</h4> */}
-                <h4>{news[2]} <span onClick={()=>{
+                <h4 onClick={()=>{
+                    setShowModal(!showModal);
+                    setSelectedTitle(news[2]);
+                }}>{news[2]} <span onClick={(event)=>{
+                    event.stopPropagation(); //전파를 막겠다
+                    // 속해있는 부모 요소의 이벤트 처리까지 전파되는걸 막겠다
+                    // <h4><span></span></h4>
 
                     //setLikeCount3(likeCount3+1);
-                    let temp = [...likeCounArr];
+                    let temp = [...likeCountArr];
                     temp[2] += 1;
                     setLikeCountArr(temp);
 
-                }}>💋</span>{likeCounArr[2]}</h4>
+                }}>💋</span>{likeCountArr[2]}</h4>
                 <p>내용 무</p>
             </div>
+            
+            {
+                // showModal == true ? <Modal/> : null
+                showModal && <Modal title={selectedTitle}/>
+            }  
             
         </div>    
     );
