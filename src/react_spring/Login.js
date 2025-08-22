3d86cccd68b66ede80005951d72d71f3c6874e17
seoungkwon <s2ngkwon2@naver.com>
2025-08-22 11:42:37 +0900

    [React Spring] JWT 방식 로그인 처리

--- src/react_spring/Login.js
+++ src/react_spring/Login.js
@@ -1,27 +1,27 @@
 import { useState } from "react";
 import axios from "axios";
 
-function Login(){
+function Login(){  
 
     let [id, setId] = useState('');
     let [pw, setPw] = useState('');
 
     return (
         <div>
-            <h1>React Spring Login</h1>
+            <h1>React Spring Loing</h1>
 
-            <p> id : <input type="text" onChange={(e)=>{ setId(e.target.value)}}></input> </p>
-            <p> pw : <input type="password" onChange={(e)=>{ setPw(e.target.value)}}></input> </p>
+            <p> id : <input type="text" onChange={(e)=>{ setId(e.target.value)} }></input>   </p>
+            <p> pw : <input type="password" onChange={(e)=>{ setPw(e.target.value)} }></input>  </p>
 
             <button onClick={()=>{
-                //로그인 시도 -> 서버레 id, pw 전달
-                
-                //axios.post( url, databody, header )
+                //로그인 시도 -> 서버에 id, pw 전달
+
+                //axios.post( url, dataBody, header )
                 axios.post(
                     '/api/login',
                     {               // post http 요청시 보낼 body data
                         id: id,     // key : value
-                        pw: pw      // id 라는 key 값 : id 변수에 들어있는 값
+                        pw: pw      // id 라는 key값 : id 변수에 들어있는 값
                     },
                     {
                         headers : {
@@ -31,18 +31,18 @@ function Login(){
                 )
                 .then(
                     response => {
-                        // { header :"", body: "" }
-                        // response.data.header.resultCode
-                        //if(response.data)
+                        // { header :"", body:""}
+                        // response.data.header.resultCode 
+
                         console.log(response.data);
-                        if( response.data == 'ok'){
+                        if( response.data == 'ok' ){
                             // 로그인 성공
-                            // 성공 보여주기
+                            // 성공 보여주기!
                             // 성공 이후 다음 페이지로 이동
-                            // ReactRouter -> navigate("/") navigate("/mypage")
+                            // ReactRouter   ->  navigate("/")     navigate("/mypage")
                         } else {
                             // 로그인 실패
-                            // 화면에 실패에 관련된 메시지를 표시
+                            // 화면에 실패에 관련된 메시지 표시
                         }
                     }
                 )
@@ -50,6 +50,7 @@ function Login(){
             }}>
                 로그인
             </button>
+
 
             <button onClick={()=>{
                 axios.post('/api/loginCheck')
@@ -59,10 +60,66 @@ function Login(){
                 로그인여부체크
             </button>
 
+
+            <button onClick={()=>{
+                //로그인 시도 -> 서버에 id, pw 전달
+
+                //axios.post( url, dataBody, header )
+                axios.post(
+                    '/api/loginJWT',
+                    {               // post http 요청시 보낼 body data
+                        id: id,     // key : value
+                        pw: pw      // id 라는 key값 : id 변수에 들어있는 값
+                    },
+                    {
+                        headers : {
+                            'Content-Type':'application/json'
+                        }
+                    }
+                )
+                .then(
+                    response => {                     
+                        console.log(response.data);  //accessToken 
+
+                        let token = response.data;   //JWT    토큰
+                        if(token != null && token != ''){
+                            //로컬 스토리지에 토큰을 저장
+                            //다음에 API 로 요청할일 있을때, 토큰도 담아서 같이 전송
+
+                            // 토큰을 로컬 스토리에 저장
+                            // localStorage.setItem(key, value)
+                            // localStorage.getItem(key)
+                            localStorage.setItem("token", token);
+                        }
+
+                        //로그인 성공? 실패?
+                        //페이지 전환 처리~ 
+                    }
+                )
+                .catch(error => console.log(error))
+            }}>
+                로그인JWT
+            </button>
+
+            <button onClick={()=>{
+                axios.post(
+                    '/api/loginCheckJWT',
+                    {},
+                    {
+                        headers: {
+                            "Content-Type":"application/json",
+                            "Authorization":"Bearer " + localStorage.getItem("token")
+                        }
+                    }
+                )
+                .then(response => console.log(response.data))
+                .catch(error => console.log(error))
+            }}>
+                로그인여부체크JWT
+            </button>
+
         </div>
     )
 }
 
 export default Login;
-
-
